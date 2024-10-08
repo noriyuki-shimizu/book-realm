6c052b49143c790e12d7984e120a18755b1ec5d1
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-10-09 00:00:22 +0900

    Nuxt Config の修正

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
+/** 本番モードか */
+const isProduction = process.env.NODE_ENV === 'production'
+
+/*
+api/health/index.get.ts
+*/
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'ja'
+      }
+    }
+  },
+  runtimeConfig: {
+    googleAiStudioApiKey: '',
+    public: {
+      isProduction
+    }
+  },
+  imports: {
+    dirs: [
+      'composables/*/index.{ts,js,mjs,mts}',
+      'utils/*/index.{ts,js,mjs,mts}'
+    ]
+  },
+  typescript: {
+    typeCheck: true,
+    tsConfig: {
+      include: ['../types/lib/index.d.ts'],
+      exclude: ['../test/**', '../**/*.spec.ts']
+    }
+  },
+  ignore: [
+    'tests/*',
+    '**/__screenshots__/*',
+    '**/__mock__/*',
+    '**/*.{spec,test}.{js,cts,mts,ts,jsx,tsx}',
+    '**/*.d.{cts,mts,ts}',
+    '**/.{output,git,cache,data}',
+    '.nuxt/analyze'
+  ]
 })
