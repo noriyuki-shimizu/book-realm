c0e8f6dadbbe229a5364c03f620fd3d441934767
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-10-15 00:47:20 +0900

    default layout を追加

---
+++ layouts/default/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+/** CSS Module */
+const cssModule = useCssModule('classes')
+</script>
+
+<template>
+  <div :class="cssModule['layout-container']">
+    <LayoutContentsHeader :class="cssModule['layout-container__header']" />
+    <main :class="cssModule['layout-container__main']">
+      <slot />
+    </main>
+    <LayoutContentsFooter :class="cssModule['layout-container__footer']" />
+  </div>
+</template>
+
+<style module="classes" lang="scss">
+@use './style.scss';
+</style>
