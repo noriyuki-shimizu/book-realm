764bda1a8fbd0ca788ce0d2d32e022c727b55e51
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-10-19 23:16:35 +0900

    OGP の設定を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+/** Runtime Config */
+const runtimeConfig = useRuntimeConfig()
+
 useHeadSafe(() => {
   const title = 'トップページ'
   const description = 'あなただけの書籍管理ができます。機能をお試しいただき、よければログインしてご利用ください。'
@@ -6,11 +9,12 @@ useHeadSafe(() => {
     title,
     meta: [
       { name: 'description', content: description },
+      { name: 'og:locale', content: 'ja_JP' },
+      { name: 'og:url', content: runtimeConfig.public.pageBaseUrl },
+      { name: 'og:image', content: `${runtimeConfig.public.pageBaseUrl}/favicon/favicon-256x256.ico` },
+      { name: 'og:site_name', content: 'BOOK Realm' },
+      { property: 'og:title', content: title },
       { property: 'og:description', content: description },
-      {
-        property: 'og:title',
-        content: title
-      }
     ]
   }
 })
