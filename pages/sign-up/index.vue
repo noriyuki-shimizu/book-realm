f1783b15ffb8a2e999f57cc4cfced76b8c2ff38f
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-10-25 14:40:59 +0900

    アカウント登録画面の枠を追加

---
+++ pages/sign-up/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+/** Runtime Config */
+const runtimeConfig = useRuntimeConfig()
+
+useHeadSafe(() => {
+  const title = 'アカウント登録'
+  const description = '書籍管理のアカウント登録フォームです。メールアドレス・パスワードのみの簡単登録！是非登録して機能をお試し下さい。'
+  return {
+    title,
+    meta: [
+      { name: 'description', content: description },
+      { name: 'og:locale', content: 'ja_JP' },
+      { name: 'og:url', content: `${runtimeConfig.public.pageBaseUrl}/sign-up` },
+      { name: 'og:image', content: `${runtimeConfig.public.pageBaseUrl}/favicon/favicon-256x256.ico` },
+      { name: 'og:site_name', content: 'BOOK Realm' },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+    ]
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1>アカウント登録</h1>
+  </div>
+</template>
