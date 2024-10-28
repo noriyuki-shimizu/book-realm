23731911d4fab241a9ca4f404671f955e9907772
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-10-29 00:24:14 +0900

    部品を追加

---
+++ functions/business/validation/error.ts
@@ -0,0 +1,16 @@
+import type { SafeParseReturnType, ZodIssue } from 'zod'
+
+/**
+ * エラーの問題配列を返す
+ *
+ * @template T - バリデーション対象の型
+ *
+ * @param {SafeParseReturnType<T, T> | null} validation - バリデーション結果
+ * @returns {ZodIssue[]} エラーの問題配列
+ */
+export const errorIssues = <T>(validation: SafeParseReturnType<T, T> | null): ZodIssue[] => {
+  if (LangUtil.isNull(validation) || validation.success) {
+    return []
+  }
+  return validation.error.issues
+}
