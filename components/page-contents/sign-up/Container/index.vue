075447566f7dc27025458bb9268d759f8549dcad
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-11-10 16:54:06 +0900

    ログイン画面への動線を追加

--- components/page-contents/sign-up/Container/index.vue
+++ components/page-contents/sign-up/Container/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SimpleForm from '../SimpleForm/index.vue'
+import AngleLeftSolidIconSvg from '@/assets/svg/angle-left-solid-icon.svg?component'
 
 /** CSS Module */
 const cssModule = useCssModule('classes')
@@ -8,7 +9,11 @@ const cssModule = useCssModule('classes')
 <template>
   <section :class="cssModule['container']">
     <h1 :class="cssModule['container__title']">アカウント登録</h1>
-    <SimpleForm />
+    <SimpleForm :class="cssModule['container__form']" />
+    <NuxtLink to="/sign-in" :class="cssModule['container__link']">
+      <AngleLeftSolidIconSvg :class="cssModule['container__icon']" />
+      <span>ログイン画面に戻る</span>
+    </NuxtLink>
   </section>
 </template>
 
