2152e5887bbd33c96c743148124883a18c844162
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-11-10 11:44:21 +0900

    登録完了画面の追加

---
+++ pages/sign-up/complete/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+/** Runtime Config */
+const runtimeConfig = useRuntimeConfig()
+
+useHeadSafe(() => {
+  const title = '登録完了'
+  const description = 'アカウント登録が完了しました！さっそく Book Realm を始めましょう！'
+  return {
+    title,
+    meta: [
+      { name: 'description', content: description },
+      { name: 'og:locale', content: 'ja_JP' },
+      { name: 'og:url', content: `${runtimeConfig.public.pageBaseUrl}/complete` },
+      { name: 'og:image', content: `${runtimeConfig.public.pageBaseUrl}/favicon/favicon-256x256.ico` },
+      { name: 'og:site_name', content: 'BOOK Realm' },
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+    ]
+  }
+})
+
+definePageMeta({
+  auth: false,
+  layout: 'simple'
+})
+</script>
+
+<template>
+  <PageContentsSignUpCompleteContainer />
+</template>
