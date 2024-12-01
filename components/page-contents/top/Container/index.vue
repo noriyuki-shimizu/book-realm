760089c4ae397fc5ecca421a0442f0ec9c94b539
noriyuki-shimizu <noriyuki.shimizu0612@gmail.com>
2024-12-01 18:41:52 +0900

    強調の文言を追加

--- components/page-contents/top/Container/index.vue
+++ components/page-contents/top/Container/index.vue
@@ -36,7 +36,7 @@ const cssModule = useCssModule('classes')
       <template #title>機能を試してみる</template>
       <template #default>
         本アプリケーションの１つ機能として、画像から書籍を解析できます。<br />
-        解析には20秒程度かかります。予めご了承ください。
+        解析には<span :class="cssModule['container__text--bold']">20秒程度</span>かかります。予めご了承ください。
       </template>
     </DescriptionSection>
 
